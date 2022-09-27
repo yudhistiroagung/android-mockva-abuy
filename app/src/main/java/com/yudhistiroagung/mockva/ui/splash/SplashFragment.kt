ba1ce0c1f51a4b2bbfc0e9880e4dab42aa74af30
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-09-27 21:09:39 +0700

    create splash and navigate to login page after 3 seconds

---
+++ app/src/main/java/com/yudhistiroagung/mockva/ui/splash/SplashFragment.kt
@@ -0,0 +1,34 @@
+package com.yudhistiroagung.mockva.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.yudhistiroagung.mockva.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_splash, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launch {
+            delay(3000L)
+            navigateToLoginPage()
+        }
+    }
+
+    private fun navigateToLoginPage() {
+        findNavController().navigate(SplashFragmentDirections.actionSplashToLoginFragment())
+    }
+
+}
