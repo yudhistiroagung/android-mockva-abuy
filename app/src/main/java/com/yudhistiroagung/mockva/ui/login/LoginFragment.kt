c52b6d26b14d82e7c6181f9ff58c7355db33365f
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-09-27 15:23:32 +0700

    initial commit and nav setup

---
+++ app/src/main/java/com/yudhistiroagung/mockva/ui/login/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.yudhistiroagung.mockva.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.yudhistiroagung.mockva.R
+import kotlinx.android.synthetic.main.fragment_login.*
+
+class LoginFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_login, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_login.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginToHomeFragment())
+        }
+    }
+
+}
