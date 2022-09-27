cc2163a74cb2f90e00ed3eae15f5c51e80749af8
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-09-27 20:55:11 +0700

    setup bottom bar

--- app/src/main/java/com/yudhistiroagung/mockva/MainActivity.kt
+++ app/src/main/java/com/yudhistiroagung/mockva/MainActivity.kt
@@ -2,10 +2,18 @@ package com.yudhistiroagung.mockva
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.app_navigation_host)
+        appBottomBar.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/yudhistiroagung/mockva/ui/account/AccountFragment.kt
@@ -0,0 +1,19 @@
+package com.yudhistiroagung.mockva.ui.account
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yudhistiroagung.mockva.R
+
+class AccountFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_account, container, false)
+
+
+}
