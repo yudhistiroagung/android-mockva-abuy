13f06a6d1f563f6ebc5faf7ef131c4275cdee102
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-09-28 15:00:33 +0700

    create confirmation history and success fragment page

---
+++ app/src/main/java/com/yudhistiroagung/mockva/presentation/ui/history/HistoryFragment.kt
@@ -0,0 +1,16 @@
+package com.yudhistiroagung.mockva.presentation.ui.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yudhistiroagung.mockva.R
+
+class HistoryFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_history, container, false)
+}
---
+++ app/src/main/java/com/yudhistiroagung/mockva/presentation/ui/transfer_confirmation/TransferConfirmationFragment.kt
@@ -0,0 +1,17 @@
+package com.yudhistiroagung.mockva.presentation.ui.transfer_confirmation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yudhistiroagung.mockva.R
+
+class TransferConfirmationFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_transfer_confirmation, container, false)
+}
