ec834216f6ad031b1963bfa40b489f5061ca0a3d
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-10-01 20:56:03 +0700

    fix basic auth encoded

--- app/src/main/java/com/yudhistiroagung/mockva/data/common/interceptor/ApiInterceptor.kt
+++ app/src/main/java/com/yudhistiroagung/mockva/data/common/interceptor/ApiInterceptor.kt
@@ -6,8 +6,8 @@ import java.io.IOException
 import javax.inject.Inject
 
 const val APIKEY = "6aa6160d-2720-4080-8673-5dbb482d4fe"
-const val SECRET = "2c68m66b9j97b2ffptvxebjy9vov5f8lhgk93g5jhgkqx4i58542o3j713cit3xz"
-const val ENCODED = "NmFhNjE2MGQtMjcyMC00MDgwLTg2NzMtNWRiYjQ4MmQ0ZmU6MmM2OG02NmI5ajk3YjJmZnB0dnhlYmp5OXZvdjVmOGxoZ2s5M2c1amhna3F4NGk1ODU0Mm8zajcxM2NpdDN4eg=="
+const val SECRET = "2c68m66b9j97b2ffptvxebjy9vov5f8lhgk93g5jhgkqx4i58524o3j713cit3xz"
+const val ENCODED = "NmFhNjE2MGQtMjcyMC00MDgwLTg2NzMtNWRiYjQ4MmQ0ZmU6MmM2OG02NmI5ajk3YjJmZnB0dnhlYmp5OXZvdjVmOGxoZ2s5M2c1amhna3F4NGk1ODUyNG8zajcxM2NpdDN4eg=="
 
 class ApiInterceptor @Inject constructor(): Interceptor {
 
